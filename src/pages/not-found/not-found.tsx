359dea1aedae8b5dfbc43233210f23a924a42fe3
Sirawat <trtor12@gmail.com>
2021-09-01 23:18:21 +0700

    cc

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound: React.FC = () => {
   return (
-    <h1>
+    <h1 style={{ marginTop: '10vh', textAlign: 'center' }}>
       <span>404 : Not Found</span> &#128567;
     </h1>
   );
